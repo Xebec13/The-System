49a91e66ecfaf339acad3b97dd2ba7535696df3c
David <dhoesen@gmail.com>
2024-03-15 19:18:36 +0100

    Almost Done

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ setInterval(() => {
     let d = new Date();
     time.innerHTML = d.toLocaleTimeString();
 }, 1000);
+
+
+let date = document.getElementById("date");
+
+setInterval(() => {
+    let dt = new Date();
+    date.innerHTML = dt.toLocaleDateString();
+}, 1000);
