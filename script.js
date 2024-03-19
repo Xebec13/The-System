ed1c7722e1388af395dbe02732253f3b8d860981
David <dhoesen@gmail.com>
2024-03-19 14:13:25 +0100

    Coding Tree Done

--- script.js
+++ script.js
@@ -12,3 +12,49 @@ setInterval(() => {
     let dt = new Date();
     date.innerHTML = dt.toLocaleDateString();
 }, 1000);
+
+// ------------------------CODING TREE JS BELOW--------------------
+let inputBx = document.querySelector("#inputBx");
+let list = document.querySelector("#list");
+
+// Wczytywanie listy zadań z Local Storage po załadowaniu strony
+document.addEventListener("DOMContentLoaded", function() {
+    loadTasks();
+});
+
+inputBx.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addItem(this.value);
+        this.value = "";
+    }
+});
+
+let addItem = (taskText) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${taskText}<i></i>`;
+    listItem.addEventListener("click", function() {
+        this.classList.toggle("done");
+        updateLocalStorage();
+    });
+    listItem.querySelector("i").addEventListener("click", function(event) {
+        event.stopPropagation();
+        listItem.remove();
+        updateLocalStorage();
+    });
+    list.appendChild(listItem);
+    updateLocalStorage();
+}
+
+// Funkcja do wczytywania listy zadań z Local Storage
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+        tasks.forEach(task => addItem(task));
+    }
+}
+
+// Funkcja do zapisywania listy zadań do Local Storage
+function updateLocalStorage() {
+    const tasks = Array.from(list.children).map(li => li.textContent.replace('×', '').trim());
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
