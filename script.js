1471fc0fd6ae201cc8444787b13b5770ff29824d
David <dhoesen@gmail.com>
2024-03-20 13:43:23 +0100

    Coding Tree DONE!

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ let inputBx = document.querySelector("#inputBx");
 let list = document.querySelector("#list");
 
 // Wczytywanie listy zadań z Local Storage po załadowaniu strony
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     loadTasks();
 });
 
-inputBx.addEventListener("keyup", function(event) {
+inputBx.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
         addItem(this.value);
         this.value = "";
@@ -32,16 +32,17 @@ inputBx.addEventListener("keyup", function(event) {
 let addItem = (taskText) => {
     let listItem = document.createElement("li");
     listItem.innerHTML = `${taskText}<i></i>`;
-    listItem.addEventListener("click", function() {
+    listItem.addEventListener("click", function () {
         this.classList.toggle("done");
         updateLocalStorage();
     });
-    listItem.querySelector("i").addEventListener("click", function(event) {
+    listItem.querySelector("i").addEventListener("click", function (event) {
         event.stopPropagation();
         listItem.remove();
         updateLocalStorage();
     });
-    list.appendChild(listItem);
+    // Wstaw nowy element na początek listy
+    list.insertBefore(listItem, list.firstChild);
     updateLocalStorage();
 }
 
@@ -49,7 +50,8 @@ let addItem = (taskText) => {
 function loadTasks() {
     const tasks = JSON.parse(localStorage.getItem('tasks'));
     if (tasks) {
-        tasks.forEach(task => addItem(task));
+        // Wczytuj zadania w odwrotnej kolejności (od najnowszego do najstarszego)
+        tasks.reverse().forEach(task => addItem(task));
     }
 }
 
@@ -58,5 +60,39 @@ function updateLocalStorage() {
     const tasks = Array.from(list.children).map(li => li.textContent.replace('×', '').trim());
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+
+const skills = document.querySelectorAll('.skill span');
+
+// Inicjalizuj sumę
+let sum1 = 0;
+
+// Przejdź przez każdy element i zsumuj punkty
+skills.forEach(skill => {
+    const points = parseInt(skill.textContent.split('/')[0]);
+    sum1 += points;
+});
+
+// Wyświetl sumę w elemencie totalPoints
+document.getElementById('totalPoints').textContent = sum1;
+
+
+const category = document.querySelectorAll('.skill span');
+
+// Inicjalizuj sumę
+let sum2 = 0;
+
+// Przejdź przez każdy element i zsumuj punkty
+skills.forEach(skill => {
+    const points = parseInt(skill.textContent.split('/')[1]);
+    sum2 += points;
+});
+
+// Wyświetl sumę w elemencie totalPoints
+document.getElementById('categoryPoints').textContent = sum2;
+
+const level = sum1 / 2;
+document.getElementById('levelPoints').textContent = level;
+
 
 // ------------------------------------CALENDAR SECTION--------------
